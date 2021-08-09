40f41eec1e03008d3bf3a1cac5ffe0b0c78789d1
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-09 16:38:30 +0530

    addinganAddress

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace realbook
+{
+    public class User
+    {
+        
+        public string _name;
+        private int _age;
+        private string _address;
+        private string _phone;
+
+        public User(string name, int age, string address, string phone)
+        {
+            _name = name;
+            _age = age;
+            _address = address;
+            _phone = phone;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace realbook
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<User> userlist = new List<User>();
+            
+            while (true)
+            {
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Write("Name  :      ");
+                        string name = Console.ReadLine();
+                        Console.Write("age  :      ");
+                        int age = int.Parse(Console.ReadLine());
+                        Console.Write("address  :      ");
+                        string address = Console.ReadLine();
+                        Console.Write("phone  :      ");
+                        string phone = Console.ReadLine();
+                        User user = new User(name, age, address, phone);
+                        userlist.Add(user);
+                        for(int i=0;i<userlist.Count;i++)
+                        {
+                            Console.WriteLine("the name is :" + userlist[i]._name);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("give the crt input");
+                        break;
+                }
+                if(option>3)
+                {
+                    break;
+                }
+                
+            }
+        }
+    }
+}
+
